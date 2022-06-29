8976a4a5c7242183743129f21e42cbece83ed195
datum-dpoulin <dpoulin@datumtc.ca>
2022-06-29 01:19:41 -0400

    Updates for CLI changes

--- dv/uvme_reset_st/src/comps/uvme_reset_st_cov_model.sv
+++ dv/uvme_reset_st/src/comps/uvme_reset_st_cov_model.sv
@@ -11,98 +11,99 @@
  * Component encapsulating Reset Self-Test Environment functional coverage model.
  */
 class uvme_reset_st_cov_model_c extends uvma_reset_cov_model_c;
-   
+
    `uvm_component_utils(uvme_reset_st_cov_model_c)
-   
+
    covergroup reset_st_cfg_cg;
       // TODO Implement reset_st_cfg_cg
       //      Ex: abc_cpt : coverpoint cfg.abc;
       is_active_cpt : coverpoint cfg.is_active;
    endgroup : reset_st_cfg_cg
-   
+
    covergroup reset_st_cntxt_cg;
       // TODO Implement reset_st_cntxt_cg
       //      Ex: abc_cpt : coverpoint cntxt.abc;
       //          xyz_cpt : coverpoint cntxt.xyz;
    endgroup : reset_st_cntxt_cg
-   
+
    covergroup reset_st_seq_item_cg;
       // TODO Implement reset_st_active_seq_item_cg
       //      Ex: abc_cpt : coverpoint active_seq_item.abc;
       //          xyz_cpt : coverpoint active_seq_item.xyz;
    endgroup : reset_st_seq_item_cg
-   
+
    covergroup reset_st_mon_trn_cg;
       // TODO Implement reset_st_passive_mon_trn_cg
       //      Ex: abc_cpt : coverpoint passive_mon_trn.abc;
       //          xyz_cpt : coverpoint passive_mon_trn.xyz;
    endgroup : reset_st_mon_trn_cg
-   
-   
+
+
    /**
     * Default constructor.
     */
    extern function new(string name="uvme_reset_st_cov_model", uvm_component parent=null);
-   
+
    /**
     * TODO Describe uvme_reset_st_cov_model_c::sample_cfg()
     */
-   extern function void sample_cfg();
-   
+   extern virtual function void sample_cfg();
+
    /**
     * TODO Describe uvme_reset_st_cov_model_c::sample_cntxt()
     */
-   extern function void sample_cntxt();
-   
+   extern virtual function void sample_cntxt();
+
    /**
     * TODO Describe uvme_reset_st_cov_model_c::sample_seq_item()
     */
-   extern function void sample_seq_item();
-   
+   extern virtual function void sample_seq_item();
+
    /**
     * TODO Describe uvme_reset_st_cov_model_c::sample_mon_trn()
     */
-   extern function void sample_mon_trn();
-   
+   extern virtual function void sample_mon_trn();
+
 endclass : uvme_reset_st_cov_model_c
 
 
 function uvme_reset_st_cov_model_c::new(string name="uvme_reset_st_cov_model", uvm_component parent=null);
-   
+
    super.new(name, parent);
-   
-   // Build covergroups
-   reset_st_cfg_cg   = new();
-   reset_st_cntxt_cg = new();
-   
+
+   reset_st_cfg_cg      = new();
+   reset_st_cntxt_cg    = new();
+   reset_st_seq_item_cg = new();
+   reset_st_mon_trn_cg  = new();
+
 endfunction : new
 
 
 function void uvme_reset_st_cov_model_c::sample_cfg();
-   
+
   reset_st_cfg_cg.sample();
-   
+
 endfunction : sample_cfg
 
 
 function void uvme_reset_st_cov_model_c::sample_cntxt();
-   
+
    reset_st_cntxt_cg.sample();
-   
+
 endfunction : sample_cntxt
 
 
 function void uvme_reset_st_cov_model_c::sample_mon_trn();
-   
-   
-   
+
+   reset_st_seq_item_cg.sample();
+
 endfunction : sample_mon_trn
 
 
 function void uvme_reset_st_cov_model_c::sample_seq_item();
-   
-   
-   
+
+   reset_st_mon_trn_cg.sample();
+
 endfunction : sample_seq_item
 
 
--- dv/uvmt_reset_st/src/tb/uvmt_reset_st_tb.sv
+++ dv/uvmt_reset_st/src/tb/uvmt_reset_st_tb.sv
@@ -11,95 +11,38 @@
  * Module encapsulating the Reset VIP Self-Test DUT wrapper, agents and clock generating interfaces.
  */
 module uvmt_reset_st_tb;
-   
+
    import uvm_pkg          ::*;
    import uvmt_reset_st_pkg::*;
-   
+
    // Clocking & Reset
    uvmt_reset_st_clk_gen_if  clk_gen_if();
-   
+
    // Agent interfaces
    uvma_reset_if  active_if (.clk(clk_gen_if.clk));
    uvma_reset_if  passive_if(.clk(clk_gen_if.clk));
-   
+
    // DUT instance
    uvmt_reset_st_dut_wrap  dut_wrap(.*);
-   
-   
+
    /**
     * Test bench entry point.
     */
    initial begin
       // Specify time format for simulation (units_number, precision_number, suffix_string, minimum_field_width)
       $timeformat(-9, 3, " ns", 18);
-      
+
       // Add interfaces to uvm_config_db
       uvm_config_db#(virtual uvmt_reset_st_clk_gen_if)::set(null, "*"                  , "clk_gen_vif", clk_gen_if);
       uvm_config_db#(virtual uvma_reset_if           )::set(null, "*.env.active_agent" , "vif"        , active_if );
       uvm_config_db#(virtual uvma_reset_if           )::set(null, "*.env.passive_agent", "vif"        , passive_if);
-      
+
       // Run test
-      uvm_top.enable_print_topology = 1;
+      uvm_top.enable_print_topology = 0;
       uvm_top.finish_on_completion  = 1;
       uvm_top.run_test();
    end
-   
-   /**
-    * End-of-test summary printout.
-    */
-   final begin
-      string             summary_string;
-      uvm_report_server  rs;
-      int                err_count;
-      int                fatal_count;
-      static bit         sim_finished = 0;
-      
-      static string  red   = "\033[31m\033[1m";
-      static string  green = "\033[32m\033[1m";
-      static string  reset = "\033[0m";
-      
-      rs          = uvm_top.get_report_server();
-      err_count   = rs.get_severity_count(UVM_ERROR);
-      fatal_count = rs.get_severity_count(UVM_FATAL);
-      
-      void'(uvm_config_db#(bit)::get(null, "", "sim_finished", sim_finished));
-      
-      $display("\n*** Test Summary ***\n");
-      
-      if (sim_finished && (err_count == 0) && (fatal_count == 0)) begin
-         $display("    PPPPPPP    AAAAAA    SSSSSS    SSSSSS   EEEEEEEE  DDDDDDD     ");
-         $display("    PP    PP  AA    AA  SS    SS  SS    SS  EE        DD    DD    ");
-         $display("    PP    PP  AA    AA  SS        SS        EE        DD    DD    ");
-         $display("    PPPPPPP   AAAAAAAA   SSSSSS    SSSSSS   EEEEE     DD    DD    ");
-         $display("    PP        AA    AA        SS        SS  EE        DD    DD    ");
-         $display("    PP        AA    AA  SS    SS  SS    SS  EE        DD    DD    ");
-         $display("    PP        AA    AA   SSSSSS    SSSSSS   EEEEEEEE  DDDDDDD     ");
-         $display("    ----------------------------------------------------------");
-         $display("                        SIMULATION PASSED                     ");
-         $display("    ----------------------------------------------------------");
-      end
-      else begin
-         $display("    FFFFFFFF   AAAAAA   IIIIII  LL        EEEEEEEE  DDDDDDD       ");
-         $display("    FF        AA    AA    II    LL        EE        DD    DD      ");
-         $display("    FF        AA    AA    II    LL        EE        DD    DD      ");
-         $display("    FFFFF     AAAAAAAA    II    LL        EEEEE     DD    DD      ");
-         $display("    FF        AA    AA    II    LL        EE        DD    DD      ");
-         $display("    FF        AA    AA    II    LL        EE        DD    DD      ");
-         $display("    FF        AA    AA  IIIIII  LLLLLLLL  EEEEEEEE  DDDDDDD       ");
-         
-         if (sim_finished == 0) begin
-            $display("    --------------------------------------------------------");
-            $display("                   SIMULATION FAILED - ABORTED              ");
-            $display("    --------------------------------------------------------");
-         end
-         else begin
-            $display("    --------------------------------------------------------");
-            $display("                       SIMULATION FAILED                    ");
-            $display("    --------------------------------------------------------");
-         end
-      end
-   end
-   
+
 endmodule : uvmt_reset_st_tb
 
 
