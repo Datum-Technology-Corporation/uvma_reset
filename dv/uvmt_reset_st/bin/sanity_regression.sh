5fd05ea36ba273fef24b118da5d475f506004758
datum-dpoulin <dpoulin@datumtc.ca>
2022-07-26 16:38:19 -0400

    updating regressions for new CLI

--- dv/uvmt_reset_st/bin/sanity_regression.sh
+++ dv/uvmt_reset_st/bin/sanity_regression.sh
@@ -1,14 +1,12 @@
 #! /bin/bash
 ########################################################################################################################
-## Copyright 2021 Datum Technology Corporation
+## Copyright 2021-2022 Datum Technology Corporation
 ## SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
 ########################################################################################################################
 
 
-# Launched from uvml project sim dir
-shopt -s expand_aliases
-source ~/.bashrc
-mio cpel    uvmt_reset_st
-mio sim     uvmt_reset_st -t rand_pulses -s 1 -c
+# Launched from uvma_reset project sim dir
+mio sim     uvmt_reset_st -CE
+mio sim     uvmt_reset_st -S -t rand_pulses -s 1 -c
 mio results uvmt_reset_st results
 mio cov     uvmt_reset_st
